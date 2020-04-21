4f058fea4847495247f93efd44d602bc039f12a2
wrunion <winterrunion@gmail.com>
2020-04-20 22:04:25 -0700

    Add logo and site title.

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -10,7 +10,3 @@ function Logo() {
 }
 
 export default Logo;
-
-{/* <Header as="h2" textAlign="center">
-<Image circular src="https://cdn.pixabay.com/photo/2015/05/07/12/17/pear-756388_1280.jpg" /> Text Here
-</Header> */}
